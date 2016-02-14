4c537d820abbadb1b12c737e22c7f3a0772c91f6
Alessio Treglia <alessio@debian.org>
2016-02-14 01:11:34 +0000

    Small improvements to output formatting

--- xkcdfinder.go
+++ xkcdfinder.go
@@ -13,11 +13,13 @@ func printResults(results []xkcd.Comic) {
 	for _, comic := range results {
 		fmt.Printf("%4d %-40s %s\n", comic.Num, comic.Title, comic.Img)
 	}
-	fmt.Println("---------")
-	fmt.Fprintf(os.Stderr,
-		"%d results, index: %d comics, latest: %d, missing: %d\n",
-		len(results), len(xkcd.ComicsIndex.Items),
-		xkcd.ComicsIndex.Latest, len(xkcd.ComicsIndex.Missing))
+	indexStats := fmt.Sprintf("%d results among %d comics, "+
+		"index stats: latest:#%d, missing=%d\n",
+		len(results),
+		len(xkcd.ComicsIndex.Items),
+		xkcd.ComicsIndex.Latest,
+		len(xkcd.ComicsIndex.Missing))
+	fmt.Fprintf(os.Stderr, indexStats)
 }
 
 func main() {
